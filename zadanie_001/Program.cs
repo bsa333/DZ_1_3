c7c4d20ef40804d3121e22e40e07bb2da4623060
bsa333 <stanislav_ayratovich@mail.ru>
2022-09-07 18:31:54 +0300

    2

--- zadanie_001/Program.cs
+++ zadanie_001/Program.cs
@@ -6,7 +6,7 @@
 Console.Clear();
 Console.WriteLine("Задача 19");
 Console.Write("Введите число: ");
-string? number = Console.ReadLine();
+string number = Console.ReadLine();
 
 void CheckingNumber(string number){
   if (number[0]==number[4] || number[1]==number[3]){
